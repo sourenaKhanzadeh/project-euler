6b87924b0c5f4f0c04676bf947982eaab66db1c9
Sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2023-03-03 01:05:19 -0500

    Solved largest prime factor

--- 1_to_50/sol.py
+++ 1_to_50/sol.py
@@ -40,12 +40,24 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 
-Answere:
+Answere: 6857
 """
 
 def largest_prime_factor(n=600851475143):
-    pass
+    factors = []
+    d = 2
+    while n > 1:
+        while n % d == 0:
+            factors.append(d)
+            n /= d
+        d += 1
+        if d * d > n:
+            if n > 1:
+                factors.append(int(n))
+            break
+    return max(factors)
 
 if __name__ == "__main__":
     # print(sum_multiples_3_5())
-    print(even_fib())
+    # print(even_fib())
+    print(largest_prime_factor())
